b231ffb27df7eb45940150308618ccd057bfc216
Sarah Allen <sarah@ultrasaurus.com>
2014-04-09 10:45:27 -0700

    allow app to start without salesforce config, just log error

--- config/initializers/salesforce.rb
+++ config/initializers/salesforce.rb
@@ -1,19 +1,25 @@
-client = Restforce.new(:host => ENV['SALESFORCE_HOST'])
+if ENV['SALESFORCE_CLIENT_SECRET'] == "secret"
+  # Rails.logger doesn't work in an initializer
+  logger = Logger.new(STDOUT)
+  logger.error("********* Salesforce not configured.  Sync will not work! *********")
+else
+  client = Restforce.new(:host => ENV['SALESFORCE_HOST'])
 
-objects = [
-		{ "SALESFORCE_CONTACT_OBJECT" => ENV['SALESFORCE_CONTACT_OBJECT'] },
-		{ "SALESFORCE_APP_OBJECT" => ENV['SALESFORCE_APP_OBJECT'] }
-	]
+  objects = [
+      { "SALESFORCE_CONTACT_OBJECT" => ENV['SALESFORCE_CONTACT_OBJECT'] },
+      { "SALESFORCE_APP_OBJECT" => ENV['SALESFORCE_APP_OBJECT'] }
+    ]
 
-objects.each do |obj|
-	obj.each_key do |key|
-		desc = client.describe(obj[key])
-		fields_arr = desc.fields.map { |f| f.name  }
-		labels_arr = desc.fields.map { |f| f.label }
-		Rails.cache.write("#{key}_FIELDS", fields_arr)
-		Rails.cache.write("#{key}_FIELD_LABELS", labels_arr)
-	end
+  objects.each do |obj|
+    obj.each_key do |key|
+      desc = client.describe(obj[key])
+      fields_arr = desc.fields.map { |f| f.name  }
+      labels_arr = desc.fields.map { |f| f.label }
+      Rails.cache.write("#{key}_FIELDS", fields_arr)
+      Rails.cache.write("#{key}_FIELD_LABELS", labels_arr)
+    end
+  end
+
+
+  SF_CLIENT = client
 end
-
-
-SF_CLIENT = client
