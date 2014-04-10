594ab1f8edf05a75d4a780e4c6e0548da7a651a3
Amos Stone <amos@amosarts.com>
2014-04-09 19:50:54 -0700

    Removes :users resource in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Brutus::Application.routes.draw do
   ActiveAdmin.routes(self)
   root :to => "home#index"
-  resources :users, :only => [:index, :show, :edit, :update ]
+
   get '/auth/:provider/callback' => 'sessions#create'
   get '/signin' => 'sessions#new', :as => :signin
   get '/signout' => 'sessions#destroy', :as => :signout
