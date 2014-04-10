7532fc84365ca7542152ec7fd915f43a15aece6b
Amos Stone <amos@amosarts.com>
2014-04-09 19:51:59 -0700

    Removes veteran status from app detail query

--- app/jobs/salesforce_sync_job.rb
+++ app/jobs/salesforce_sync_job.rb
@@ -13,10 +13,8 @@ class SalesforceSyncJob
         detail = @client.query("select #{Rails.cache.read('SALESFORCE_APP_OBJECT_FIELDS').join(", ")} from PIF_Application__c where PIF_Contact__c = '#{contact['Id']}'").first
         projects = @client.query("select Id, IsDeleted, Name, CreatedDate, CreatedById, LastModifiedDate, LastModifiedById, SystemModstamp, PIF_Application__c, PIF_Project_Name__c, Project_Fit__c from PIF_Project__c where PIF_Application__c = '#{detail['Id']}'")
         projects.each do |project|
-          # new_app.project_list.add(project['PIF_Project_Name__c'])
           new_app.tag_list.add(project['PIF_Project_Name__c'])
         end
-        new_app.vet_status = detail['Veteran_Status__c']
         new_app.tag_list = detail['Skills__c'].split(';').join(', ') if detail['Skills__c']
         new_apps << new_app
         new_app.save
