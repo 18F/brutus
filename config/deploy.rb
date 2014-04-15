8a4e6af30d090fc725566889216ec2d321f04b5c
Amos Stone <amos@amosarts.com>
2014-04-15 16:07:35 -0700

    Removes unncessary link for redis

--- config/deploy.rb
+++ config/deploy.rb
@@ -16,7 +16,7 @@ set :log_level, :debug
 # set :ssh_options, { :forward_agent => true }
 set :resque_environment_task, true
 
-set :linked_files, %w{config/database.yml config/application.yml config/redis/qa.conf config/newrelic.yml}
+set :linked_files, %w{config/database.yml config/application.yml config/newrelic.yml}
 set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets public/system}
 
 # EWW THIS IS UGLY
